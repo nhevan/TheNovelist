c4ee540b55fa8f455a7cc361e402d3bece1767c0
NH Evan <nhevan@gmail.com>
2017-12-13 13:59:30 +0600

    New AngleCaclculator can now find the angle between the primary and x-axis, this is the angle that the primary hand needs to move to reach a particular point.

---
+++ app/AngleCalculator.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App;
+
+class AngleCalculator
+{
+	/**
+	 * the length of the primary hand in cm
+	 * @var [type]
+	 */
+	protected $primary_hand_length;
+
+	/**
+	 * the length of the secondary hand in cm
+	 * @var [type]
+	 */
+	protected $secondary_hand_length;
+
+	/**
+	 * the x coordinate of the target point
+	 * @var [type]
+	 */
+	protected $x;
+
+	/**
+	 * the y coordinate of the target point
+	 * @var [type]
+	 */
+	protected $y;
+
+    /**
+     * @return mixed
+     */
+    public function getPrimaryHandLength()
+    {
+        return $this->primary_hand_length;
+    }
+
+    /**
+     * @param mixed $primary_hand_length
+     *
+     * @return self
+     */
+    public function setPrimaryHandLength($primary_hand_length)
+    {
+        $this->primary_hand_length = $primary_hand_length;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecondaryHandLength()
+    {
+        return $this->secondary_hand_length;
+    }
+
+    /**
+     * @param mixed $secondary_hand_length
+     *
+     * @return self
+     */
+    public function setSecondaryHandLength($secondary_hand_length)
+    {
+        $this->secondary_hand_length = $secondary_hand_length;
+
+        return $this;
+    }
+
+    /**
+     * sets the coordinates of the point
+     * @param [type] $x [description]
+     * @param [type] $y [description]
+     */
+    public function setPoint($x, $y)
+    {
+    	$this->x = $x;
+    	$this->y = $y;
+    }
+
+    /**
+     * returns the distance of the target point from the origin
+     * @return [type] [description]
+     */
+    public function getDistance()
+    {
+    	return sqrt( ( $this->x * $this->x ) + ( $this->y * $this->y ) );
+    }
+
+    /**
+     * calculates and returns the area of the arbitrary triangle formed by the target point and the two hands
+     * @return [type] [description]
+     */
+    public function getArea()
+    {
+    	$perimeter = $this->getPrimaryHandLength() + $this->getSecondaryHandLength() + $this->getDistance();
+    	$s = $perimeter / 2;
+    	$area = sqrt($s * ($s - $this->getPrimaryHandLength()) * ($s - $this->getSecondaryHandLength()) * ($s - $this->getDistance()));
+
+    	return $area;
+    }
+
+    /**
+     * calculates and returns the height of the arbitrary triangle formed by the target point and the two sides
+     * @return [type] [description]
+     */
+    public function getTriangleHeight()
+    {
+    	$area = $this->getArea();
+    	$height = (2 * $area) / $this->getDistance();
+
+    	return $height;
+    }
+
+    /**
+     * returns the angle between the target line and primary hand on the arbitrary triangle 
+     * @return [type] [description]
+     */
+    public function getTargetToPrimaryAngle()
+    {
+    	$p = $this->getTriangleHeight();
+    	$h = $this->getPrimaryHandLength();
+    	$ratio = $p / $h;
+    	
+    	return rad2deg(asin($ratio));
+    }
+
+    /**
+     * returns the angle between the target and x axis
+     * @return [type] [description]
+     */
+    public function getTargetToXAxisAngle()
+    {
+    	$p = $this->y;
+    	$b = $this->x;
+    	$ratio = $p / $b;
+    	
+    	return rad2deg(atan($ratio));
+    }
+
+    /**
+     * returs the angle between primary hand and x axis
+     * @return [type] [description]
+     */
+    public function getPrimaryHandAngle()
+    {
+    	return $this->getTargetToXAxisAngle() - $this->getTargetToPrimaryAngle();
+    }
+}
---
+++ tests/Unit/AngleCalculatorTest.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\AngleCalculator;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AngleCalculatorTest extends TestCase
+{
+	protected $calculator;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->calculator = new AngleCalculator;
+	}
+
+    /**
+     * @test
+     * it can set primary hand length
+     */
+    public function it_can_set_primary_hand_length()
+    {
+        //act
+        $primary_hand_length = $this->calculator->setPrimaryHandLength(15)->getPrimaryHandLength();
+    
+        //assert
+        $this->assertEquals(15, $primary_hand_length);
+    }
+
+    /**
+     * @test
+     * it can set secondary hand length
+     */
+    public function it_can_set_secondary_hand_length()
+    {
+        //act
+        $secondary_hand_length = $this->calculator->setSecondaryHandLength(15)->getSecondaryHandLength();
+    
+        //assert
+        $this->assertEquals(15, $secondary_hand_length);
+    }
+
+    /**
+     * @test
+     * it can calculate the distance of a given point from the origin
+     */
+    public function it_can_calculate_the_distance_of_a_given_point_from_the_origin()
+    {
+    	//arrange
+    	$x = 30;
+    	$y = 20;
+    	$this->calculator->setPoint($x, $y);
+    
+        //act
+    	$distance = $this->calculator->getDistance();
+    
+        //assert
+        $this->assertEquals(36.05551, $distance, '', 0.00001);
+    }
+
+    /**
+     * @test
+     * it can calculate the area of the arbitrary triangle created by the point and the two hands
+     */
+    public function it_can_calculate_the_area_of_the_arbitrary_triangle_created_by_the_point_and_the_two_hands()
+    {
+    	//arrange
+    	$this->calculator->setPrimaryHandLength(15);
+        $this->calculator->setSecondaryHandLength(15);
+    	$this->calculator->setPoint(10.60660, 10.60660); // a point with a distance of 15cm from the origin
+
+        //act
+    	$area = $this->calculator->getArea();
+    
+        //assert
+        $this->assertEquals(97.42785, $area, '', 0.00001);
+    }
+
+    /**
+     * @test
+     * it can find the height of the arbitrary triangle
+     */
+    public function it_can_find_the_height_of_the_arbitrary_triangle()
+    {
+    	//arrange
+        $this->calculator->setPrimaryHandLength(15);
+        $this->calculator->setSecondaryHandLength(15);
+    	$this->calculator->setPoint(10.60660, 10.60660); // a point with a distance of 15cm from the origin
+    
+        //act
+    	$height = $this->calculator->getTriangleHeight();
+    
+        //assert
+        $this->assertEquals(12.99038, $height, '', 0.00001);
+    }
+
+    /**
+     * @test
+     * it can calculate the angle formed by the target point line and primary hand
+     */
+    public function it_can_calculate_the_angle_formed_by_the_target_point_line_and_primary_hand()
+    {
+    	//arrange
+    	$this->calculator->setPrimaryHandLength(15);
+        $this->calculator->setSecondaryHandLength(15);
+    	$this->calculator->setPoint(10.60660, 10.60660); // a point with a distance of 15cm from the origin
+    
+        //act
+    	$angle = $this->calculator->getTargetToPrimaryAngle();
+
+        //assert
+        $this->assertEquals(60.000000, $angle, '', 0.00001); // each angle of a equilateral triangle has a angle of 60 deg
+    }
+
+    /**
+     * @test
+     * it can calculate the angle formed by the target point line and x axis
+     */
+    public function it_can_calculate_the_angle_formed_by_the_target_point_line_and_x_axis()
+    {
+    	//arrange
+    	$this->calculator->setPrimaryHandLength(15);
+        $this->calculator->setSecondaryHandLength(15);
+    	$this->calculator->setPoint(10.60660, 10.60660); // a point with a distance of 15cm from the origin
+    
+        //act
+    	$angle = $this->calculator->getTargetToXAxisAngle();
+
+        //assert
+        $this->assertEquals(45.000000, $angle, '', 0.00001); // each angle of a equilateral triangle has a angle of 60 deg
+    }
+
+    /**
+     * @test
+     * it can pass neelas first scenario
+     */
+    public function it_can_pass_neelas_first_scenario()
+    {
+    	//arrange
+        $this->calculator->setPrimaryHandLength(17.2);
+    	$this->calculator->setSecondaryHandLength(13.9);
+    	$this->calculator->setPoint(2.6, 12.7);
+
+        //act
+    	$angle = $this->calculator->getTargetToPrimaryAngle();
+
+        //assert
+        $this->assertEquals(52, floor($angle));
+    }
+
+    /**
+     * @test
+     * it can pass neelas second scenario
+     */
+    public function it_can_pass_neelas_second_scenario()
+    {
+    	//arrange
+        $this->calculator->setPrimaryHandLength(16.4);
+    	$this->calculator->setSecondaryHandLength(8.2);
+    	$this->calculator->setPoint(7.7, 20.5);
+
+        //act
+    	$angle = $this->calculator->getTargetToPrimaryAngle();
+
+        //assert
+        $this->assertEquals(18.5, round($angle, 1));
+    }
+
+    /**
+     * @test
+     * it can return the primary hand angle with x axis
+     */
+    public function it_can_return_the_primary_hand_angle_with_x_axis()
+    {
+    	//arrange
+        $this->calculator->setPrimaryHandLength(15);
+        $this->calculator->setSecondaryHandLength(15);
+    	$this->calculator->setPoint(10.60660, 10.60660); // a point with a distance of 15cm from the origin
+    
+        //act
+    	$angle = $this->calculator->getPrimaryHandAngle();
+    
+        //assert
+        $this->assertEquals(15, round($angle));
+    }
+}
